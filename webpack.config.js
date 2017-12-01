066f2f18a6b76afd6855bdd40032d06c4bd9a49b
deng1234 <d13588345401@163.com>
2017-12-01 12:19:22 +0800

    minimize bundle.js and production enviroment

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 var path = require("path");
+var webpack = require('webpack');
 
 module.exports = {
 	entry: "./src/index.js",
-    devtool:"cheap-module-source-map",
+    devtool: "#cheap-module-eval-source-map",
 	output: {
 		path: path.join(__dirname, "./dist"),
 		publicPath: "/dist/",
@@ -36,4 +37,20 @@ module.exports = {
             'vue': 'vue/dist/vue.js'
         }
     }
+}
+
+if(process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#cheap-module-source-map';
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ])
 }
